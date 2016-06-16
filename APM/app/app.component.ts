18c98d54aecf7fb95f570971e90fddfee1f0e2ff
trojanhorser <denisdbell@gmail.com>
2016-06-15 21:15:22 -0500

    added product list table

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core' 
-
+import {ProductListComponent} from './products/product-list.component'
 @Component({
     selector: 'pm-app',
     template: 
     `<div>
          <h1>{{pageTitle}}</h1>
-         <div> My First Component </div>
-    </div>`
+         <pm-products></pm-products>
+    </div>`,
+    directives: [ProductListComponent]
     
 })
 
