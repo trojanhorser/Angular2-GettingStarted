b51496d801f6f6f45eab58d575c0f59b78a310c9
trojanhorser <denisdbell@gmail.com>
2016-06-19 20:08:33 -0500

    Added http functionality to product service

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from 'angular2/core' 
+import {HTTP_PROVIDERS} from 'angular2/http'
+import 'rxjs/Rx';
+
 import {ProductListComponent} from './products/product-list.component'
+import {ProductService} from './products/product.service' 
+
 @Component({
     selector: 'pm-app',
     template: 
@@ -7,8 +12,8 @@ import {ProductListComponent} from './products/product-list.component'
          <h1>{{pageTitle}}</h1>
          <pm-products></pm-products>
     </div>`,
-    directives: [ProductListComponent]
-    
+    directives: [ProductListComponent],
+    providers: [ProductService,HTTP_PROVIDERS]
 })
 
 export class AppComponent{
