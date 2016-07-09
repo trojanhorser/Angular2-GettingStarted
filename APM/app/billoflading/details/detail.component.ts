685def04a69afa983b3a3c3e9730fda7b8ad3659
trojanhorser <denisdbell@gmail.com>
2016-07-09 00:37:05 -0500

    Added features to BL Details

--- APM/app/billoflading/details/detail.component.ts
+++ APM/app/billoflading/details/detail.component.ts
@@ -6,5 +6,26 @@ import {Component} from 'angular2/core'
 })
 
 export class BillOfLadingDetailComponent{
-    
+     
+     billOfLadingNumber: Number;
+     masterBillOfLadingNumber: Number;
+     billOfLadingNumberImport: Number;
+     warfCode: Number;
+     originPort: String;
+     destination: String; 
+     loadingPort: String; 
+     discharge: String; 
+     consigneePort: String; 
+     shipper: String; 
+     notify: String;
+     
+     showMaximizeIcon: Boolean = false;
+     showCalapseIcon: Boolean = true;
+     showWidgetBody: Boolean = true;
+     
+     onMinimizeClicked(event): void{
+        this.showMaximizeIcon = !this.showMaximizeIcon;
+        this.showCalapseIcon = !this.showCalapseIcon;
+        this.showWidgetBody = !this.showWidgetBody;
+    }
 }
